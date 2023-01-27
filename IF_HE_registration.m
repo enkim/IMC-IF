7c9edc9402bbcbbc75a060ce0a4a6e038bf41178
Eun Na Kim <31251305+enkim@users.noreply.github.com>
2023-01-27 23:44:11 +0900

    Add files via upload

---
+++ IF_HE_registration.m
@@ -0,0 +1,176 @@
+% IF - H&E ROI registration 
+% New dataset
+clear all;
+clc;
+close all;
+
+% image path
+HE_path = './1. Brightfield/3. HE/';
+
+% temporarily I used H&E path
+
+%%%%% need to update %%%%%%%%%%%%
+% 지금은 일단 mask path 가 없어서 그냥 HE_path 값으로 대체했습니다. 
+% mask image 경로를 나중에 넣어주면 됩니다. 
+HE_mask_path = HE_path; % H&E mask image directory (* need to update) 
+%%%%% need to update %%%%%%%%%%%%
+
+
+IF_path = './2. IF/';
+IMC_path = './3. TIFF/';
+output_path = './registered_HE/';
+mkdir(output_path);
+
+
+HE_filename = dir(sprintf('%s*.tif', HE_path));
+HE_mask_filename = dir(sprintf('%s*.tif', HE_mask_path)); 
+IF_filename = dir(sprintf('%s*.tif', IF_path));
+
+
+% parameter
+HE_scan = 0.22; % umm/pxl 
+IMC_scan = 1.0; % umm/pxl
+IF_scan = 0.325; % umm/pxl
+
+
+
+%% image reading
+I_HE = imread(sprintf('%s%s',HE_path,HE_filename.name));
+I_HE_mask = imread(sprintf('%s%s',HE_mask_path,HE_mask_filename.name));
+
+%%%%% need to update %%%%%%%%%%%%
+% 이부분은 나중에 지워 주시면 됩니다. 현재는 mask image 가 없어서 H&E 로 사용하기 때문에
+% single channel image 로 넣도록 했습니다. 
+I_HE_mask = I_HE_mask(:,:,1);
+%%%%% need to update %%%%%%%%%%%%
+
+
+I_IF = imread(sprintf('%s%s',IF_path, IF_filename.name));
+
+
+% image resize
+I_IF_resize = imresize(I_IF, IF_scan/IMC_scan);
+I_IF_resize = imrotate(I_IF_resize,180);
+
+I_HE_resize = imresize(I_HE, HE_scan/IMC_scan); %HE_scan/IF_scan); 
+I_HE_resize = imrotate(I_HE_resize,180);
+
+I_HE_mask_resize = imresize(I_HE_mask, HE_scan/IMC_scan);
+I_HE_mask_resize = imrotate(I_HE_mask_resize,180);
+
+
+
+%% registration
+
+% reference image
+IF_DAPI = I_IF_resize(:,:,1);
+I_ref = imbinarize(IF_DAPI, graythresh(IF_DAPI)*0.5); % reference IF DAPI
+%ptsRef_all = detectSURFFeatures(I_ref);
+%ptsRef=ptsRef_all.selectStrongest(N_smpl);
+
+
+I_HE_gray = (rgb2gray(I_HE_resize));
+I_HE_obj = imcomplement(imbinarize( I_HE_gray, graythresh(I_HE_gray)*0.65));
+
+I_HE_BD = imfill(imerode(I_HE_gray > 0, strel('disk',20)), 'holes');
+
+I_HE_obj( find(I_HE_BD == 0)) = 0;
+
+I_obj = I_HE_obj;
+
+%%
+%%
+% correlation based
+
+%I_ref_org = I_ref;
+%I_ref = zeros(floor(size(I_obj,1)*1.25), floor(size(I_obj,2)*1.25))>0; % make sure reference is bigger than obj
+%I_ref(1:size(I_ref_org,1), 1:size(I_ref_org,2)) = I_ref_org;
+
+
+c = normxcorr2( I_obj, I_ref);
+%figure, surf(c), shading flat
+[max_c, imax] = max(abs(c(:)));
+[ypeak, xpeak] = ind2sub(size(c),imax(1));
+corr_offset = [(xpeak-size(I_obj,2)) 
+           (ypeak-size(I_obj,1))];
+
+% total offset
+offset = corr_offset;
+xoffset = max(offset(1),0);
+yoffset = max(offset(2),0);
+
+xbegin = round(xoffset+1);
+xend   = round(xoffset+ size(I_obj,2));
+ybegin = round(yoffset+1);
+yend   = round(yoffset+size(I_obj,1));
+
+registered_obj = (zeros(size(I_ref)));
+registered_obj(ybegin:yend, xbegin:xend,:) = I_obj;
+registered_obj = registered_obj > 0;
+
+registered_mask = (zeros(size(I_ref)));
+registered_mask(ybegin:yend, xbegin:xend,:) = I_HE_mask_resize;
+
+
+registered_HE = zeros(size(I_ref,1),size(I_ref,2),3);
+for ch=1:3
+    registered_HE(ybegin:yend, xbegin:xend,ch)= I_HE_resize(:,:,ch);
+end
+
+
+I_over = imfuse(I_ref, registered_obj, ...
+                              'falsecolor', 'Scaling','joint', 'ColorChannels', [1 2 0]);
+figure; imagesc(I_over)
+
+
+%%
+scale = 0.25;
+I_ref_scale = imresize(I_ref,scale); % temp
+registered_obj_scale = imresize(registered_obj, scale); % temp
+
+
+moving_scale = uint8(registered_obj_scale);
+fixed_scale = uint8(I_ref_scale);
+[optimizer,metric] = imregconfig('multimodal');
+%movingRegisteredDefault = imregister(moving,fixed,'similar',optimizer,metric);
+
+optimizer.MaximumIterations = 500;
+optimizer.InitialRadius = 3.5e-3;
+%optimizer.InitialRadius = optimizer.InitialRadius/3;
+
+
+%[movingRegisteredAdjustedInitialRadius, tform] = imregister(moving,fixed,'rigid',optimizer,metric);
+
+tform_scale = imregtform(moving_scale, fixed_scale, 'similar', optimizer, metric);
+
+movingReg_scale = imwarp(moving_scale, tform_scale, 'OutputView',imref2d(size(fixed_scale)));
+
+
+tform = tform_scale;
+
+tform.T(3,1) = tform_scale.T(3,1)/scale;
+tform.T(3,2) = tform_scale.T(3,2)/scale;
+
+
+fixed = uint8(I_ref);
+moving = uint8(registered_obj);
+movingReg = imwarp(moving, tform, 'OutputView', imref2d(size(fixed)));
+
+I_over2 = imfuse( fixed, movingReg, ...
+                              'falsecolor', 'Scaling','joint', 'ColorChannels', [1 2 0]);
+
+figure; imagesc(I_over2)
+
+reg_mask = imwarp(registered_mask, tform, 'OutputView', imref2d(size(fixed)));
+
+registered_HE_final = zeros(size(I_ref,1),size(I_ref,2),3);
+for ch=1:3
+    reg_HE_final(:,:,ch) = imwarp(registered_HE(:,:,ch), tform, 'OutputView', imref2d(size(fixed)));
+end
+
+
+%imwrite( imrotate(uint8(reg_mask),-180), sprintf('%sRegistered_HE_mask_%s', output_path,HE_filename.name), 'Tiff');
+
+imwrite( imrotate(uint8(reg_HE_final),-180), sprintf('%sRegistered_HE_%s', output_path,HE_filename.name), 'Tiff');
+    
+
---
+++ IF_IMC_ROI_registration_31_40.m
@@ -0,0 +1,153 @@
+% IF - IMC ROI registration 
+% New dataset
+clear all;
+clc;
+close all;
+
+% image path
+HE_path = './1. Brightfield/3. HE/';
+IF_path = './2. IF/';
+IMC_path = './3. TIFF/';
+output_path = './registered_IMC/';
+mkdir(output_path);
+
+
+
+HE_filename = dir(sprintf('%s*.tif', HE_path));
+IF_filename = dir(sprintf('%s*.tif', IF_path));
+
+
+% parameter
+HE_scan = 264.5833*10^-3; % umm/pxl 
+IMC_scan = 1.0; % umm/pxl
+IF_scan = 0.325; % umm/pxl
+N_smpl = 2000; % num. of feature pts.
+    
+%% image reading
+%I_HE = imread(sprintf('%s%s',HE_path,HE_filename.name));
+I_IF = imread(sprintf('%s%s',IF_path, IF_filename.name));
+
+
+% image resize
+I_IF_resize = imresize(I_IF, IF_scan/IMC_scan);
+I_IF_resize = imrotate(I_IF_resize,180);
+
+%I_HE_resize = imresize(I_HE, HE_scan/IMC_scan); 
+%I_HE_resize = imrotate(I_HE_resize,180);
+
+% reference feature
+IF_DAPI = I_IF_resize(:,:,1);
+IF_CK = I_IF_resize(:,:,2);
+IF_CD45 = I_IF_resize(:,:,3);
+
+
+I_ref = imbinarize(IF_DAPI, graythresh(IF_DAPI)); % reference IF DAPI
+ptsRef_all = detectSURFFeatures(I_ref);
+%ptsRef=ptsRef_all.selectStrongest(N_smpl*5);
+
+
+%%
+% ROI
+roi_name = dir(sprintf('%sROI*', IMC_path));
+
+marker_name = dir(sprintf('%s%s/*.tiff', IMC_path,roi_name(1).name));
+
+for ch=31:40; %length(marker_name)
+    %ch = 44;
+    
+    fprintf('marker: %s\n', marker_name(ch).name);
+    
+    
+    Registered_IMC = zeros( size(I_ref));
+    for r=1:length(roi_name)
+      
+
+        fprintf('ROI: %d\n', r);
+
+        % find DNA image
+        I_IMC_DNA = [];
+        IMC_DNA_name = dir(sprintf('%s%s/*DNA.ome.tiff', IMC_path, roi_name(r).name));
+        I_IMC_DNA(:,:,1) = imread(sprintf('%s%s/%s', IMC_path, roi_name(r).name, IMC_DNA_name(1).name));
+        I_IMC_DNA(:,:,2) = imread(sprintf('%s%s/%s', IMC_path, roi_name(r).name, IMC_DNA_name(2).name));
+        I_obj = max(I_IMC_DNA(:,:,1), I_IMC_DNA(:,:,2));
+
+        I_obj = imadjust(uint8(I_obj/max(I_obj(:))*255));
+        I_obj = imbinarize(I_obj,graythresh(I_obj));
+
+        
+        % read IMC image
+        I_IMC = [];
+        I_IMC = imread(sprintf('%s%s/%s', IMC_path, roi_name(r).name,marker_name(ch).name));
+
+        % correlation based
+        c = normxcorr2( I_obj, I_ref);
+        %figure, surf(c), shading flat
+        [max_c, imax] = max(abs(c(:)));
+        [ypeak, xpeak] = ind2sub(size(c),imax(1));
+        corr_offset = [(xpeak-size(I_obj,2)) 
+                   (ypeak-size(I_obj,1))];
+
+        % total offset
+        offset = corr_offset;
+        xoffset = offset(1);
+        yoffset = offset(2);
+
+        xbegin = round(xoffset+1);
+        xend   = round(xoffset+ size(I_obj,2));
+        ybegin = round(yoffset+1);
+        yend   = round(yoffset+size(I_obj,1));
+
+        registered_obj = (zeros(size(I_ref)));
+        registered_obj(ybegin:yend, xbegin:xend,:) = I_obj;
+        registered_obj = registered_obj > 0;
+
+        registered_IMC = (zeros(size(I_ref)));
+        registered_IMC(ybegin:yend, xbegin:xend,:) = I_IMC;
+       % imagesc(imfuse(I_ref, registered_obj, 'falsecolor', 'Scaling','joint', 'ColorChannels', [1 2 0]));
+
+
+        %
+
+        ptsObj_all = []; ptsObj = [];
+        ptsObj_all = detectSURFFeatures(registered_obj);
+        %ptsObj =ptsObj_all.selectStrongest(N_smpl);
+
+
+        xv = [ xbegin xbegin xend xend]';
+        yv = [ yend ybegin ybegin yend]';
+        % select feature within location
+
+        ptsObj = ptsObj_all( inpolygon( ptsObj_all.Location(:,1), ptsObj_all.Location(:,2), xv, yv));
+        ptsRef = ptsRef_all( inpolygon( ptsRef_all.Location(:,1), ptsRef_all.Location(:,2), xv, yv));
+
+
+
+        [featuresRef, validPtsRef] = extractFeatures( I_ref, ptsRef);
+        [featuresObj, validPtsObj] = extractFeatures( registered_obj, ptsObj);
+
+        indxPairs = matchFeatures(featuresRef, featuresObj);
+        matchedRef = validPtsRef(indxPairs(:,1));
+        matchedObj = validPtsObj(indxPairs(:,2));
+
+        [tform, inlierDistorted, inlierOriginal,status] = estimateGeometricTransform(...
+              matchedObj, matchedRef, 'similarity', 'MaxNumTrials',500, 'Confidence',99.00, 'MaxDistance',5);
+
+%          I_over = imfuse(I_ref, imwarp(registered_obj, tform, 'OutputView', imref2d(size(I_ref))), ...
+%                              'falsecolor', 'Scaling','joint', 'ColorChannels', [1 2 0]);
+
+
+        registered_IMC = imwarp(registered_IMC, tform, 'OutputView', imref2d(size(I_ref)));
+        
+        
+        % Using max projection
+        Registered_IMC = max( registered_IMC, Registered_IMC);
+                         
+        %imagesc(I_over);
+        fprintf('ROI %d registration done\n', r);
+    end
+    
+    imwrite( imrotate(uint16(Registered_IMC),-180), sprintf('%sRegistered_%s', output_path, marker_name(ch).name), 'Tiff');
+    
+    % checking
+    % imagesc(imfuse(I_ref, imbinarize(Registered_IMC, graythresh(Registered_IMC)), 'falsecolor', 'Scaling','joint', 'ColorChannels', [1 2 0]));
+end
